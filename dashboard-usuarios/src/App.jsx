a06fbc69748b9473deccc34a954d0fd29fdbc423
Simone Lacerda <simone.lacerda@acad.bentoquirino.edu>
2025-05-13 15:41:11 -0300

    Início da paginação

--- dashboard-usuarios/src/App.jsx
+++ dashboard-usuarios/src/App.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react'
 import UserCard from './components/UserCard';
 import './App.css'
+import Pagination from './components/Pagination';
+
+let PageSize = 10;
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -12,6 +16,12 @@ function App() {
       .catch((err) => console.error('Erro ao buscar usuários:', err));
   }, []);
 
+  const currentUserCard = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return data.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+  
   return (
     <div className='App'>
       <h1>Dashboard de Usuários</h1>
@@ -19,6 +29,13 @@ function App() {
       <div className='user-container'>
         {users.map((user) => (
           <UserCard key={user.id} user={user} />
+          <Pagination
+            className="pagination-bar"
+            currentPage={currentPage}
+            totalCount={data.length}
+            pageSize={PageSize}
+            onPageChange={page => setCurrentPage(page)}
+          />
         ))}
       </div>
     </div>
